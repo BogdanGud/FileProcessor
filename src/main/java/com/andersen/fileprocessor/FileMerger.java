e60022c14a9d7a206097facb71900e2dd9ba0e4c
BogdanGud <bogdan.gudzulyak@gmail.com>
2017-01-02 22:09:18 +0200

    refactoring, deleted hardcode

--- src/main/java/com/andersen/fileprocessor/FileMerger.java
+++ src/main/java/com/andersen/fileprocessor/FileMerger.java
@@ -10,40 +10,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileMerger {
-
-
     private final static Charset ENCODING = StandardCharsets.UTF_8;
     private List<String> strings = new ArrayList<>();
 
-    public void mergeFile(String inputFile1, String inputFile2, String outputFile) {
-        Path pathFile1 = Paths.get(inputFile1);
-        Path pathFile2 = Paths.get(inputFile2);
-        Path output = Paths.get(outputFile);
+    public void mergeFile(String[] args) {
+        Path pathFile1 = Paths.get(args[0]);
+        Path pathFile2 = Paths.get(args[1]);
+        Path output = Paths.get(args[2]);
         try {
             BufferedReader reader1 = Files.newBufferedReader(pathFile1, ENCODING);
             BufferedReader reader2 = Files.newBufferedReader(pathFile2, ENCODING);
             String line = null;
-           // String line2 = null;
+            String line2 = null;
 
-            while ((line = reader1.readLine()) != null) { //&& (line2 = reader2.readLine()) != null) {
+            while ((line = reader1.readLine()) != null && (line2 = reader2.readLine()) != null) {
                 strings.add(line);
-               // strings.add(line2);
+                strings.add(line2);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        System.out.println(strings.toString());
 
-        try {
-            BufferedWriter writer = Files.newBufferedWriter(output, ENCODING);
+        try (BufferedWriter writer = Files.newBufferedWriter(output, ENCODING)) {
             for (String line : strings) {
-                writer.write(line);
+                writer.write(line.toString());
                 writer.newLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
 }
--- src/main/java/com/andersen/fileprocessor/Main.java
+++ src/main/java/com/andersen/fileprocessor/Main.java
@@ -4,12 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        final String inputFile1 = "C:\\TMP\\input1.txt";
-        final String inputFile2 = "C:\\TMP\\input2.txt";
-        final String outputFile = "C:\\TMP\\output.txt";
         FileMerger merger = new FileMerger();
-        merger.mergeFile(inputFile1, inputFile2, outputFile);
-
-
+        merger.mergeFile(args);
     }
 }
